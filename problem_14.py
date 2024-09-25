71e874623c9adba046f057fd6d8d0215bad996b3
ebrahimhossaincse <mdebrahimhossain.me@gmail.com>
2024-09-25 02:51:43 -0400

    problem 14

---
+++ problem_14.py
@@ -0,0 +1,4 @@
+'''
+Write a Python program that takes the time in hours (24-hour format) as input
+and prints “Good Morning”, “Good Afternoon”, “Good Evening”, or “Good Night” based on the time.
+'''
