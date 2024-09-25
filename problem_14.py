394f579367d15008a3b4bb684f93b9acd284b292
ebrahimhossaincse <mdebrahimhossain.me@gmail.com>
2024-09-25 02:52:12 -0400

    problem 14

--- problem_14.py
+++ problem_14.py
@@ -2,3 +2,6 @@
 Write a Python program that takes the time in hours (24-hour format) as input
 and prints “Good Morning”, “Good Afternoon”, “Good Evening”, or “Good Night” based on the time.
 '''
+# Import the custom dialog from the other module
+from custom_dialog import CustomDialog, root, messagebox
+
