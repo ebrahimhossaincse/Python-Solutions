62078458cb826245cdcf4047b3ab3271bcaa2c50
ebrahimhossaincse <mdebrahimhossain.me@gmail.com>
2024-09-25 01:26:29 -0400

    problem 12

---
+++ problem_12.py
@@ -0,0 +1,8 @@
+'''
+Write a Python program that takes a student’s percentage as input and prints their corresponding grade according to the following criteria:
+– 90% or above: A+
+– 80-89%: A
+– 70-79%: B
+– 60-69%: C
+– Below 60%: Fail
+'''
