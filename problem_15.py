8e5e94201ee182023d51c71ba0e7f264fe2cc38c
ebrahimhossaincse <mdebrahimhossain.me@gmail.com>
2024-09-25 03:17:06 -0400

    problem 15

---
+++ problem_15.py
@@ -0,0 +1,7 @@
+'''
+Write a Python program that takes three sides of a triangle as input and determines whether it forms an equilateral, isosceles, or scalene triangle.
+'''
+
+# Import the custom dialog from the other module
+from custom_dialog import CustomDialog, root, messagebox
+
