d4e6795e8f7182ae04d93d3dd0da500349ea6acf
ebrahimhossaincse <mdebrahimhossain.me@gmail.com>
2024-09-25 07:16:28 -0400

    problem 19

---
+++ problem_19.py
@@ -0,0 +1,5 @@
+"""
+Write a Python program using a while loop to calculate the factorial of a given number N
+"""
+# Import the custom dialog from the other module
+from custom_dialog import CustomDialog, root, messagebox
